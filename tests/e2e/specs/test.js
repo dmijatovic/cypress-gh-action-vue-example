5e8387de7a3ce686f59af62b0e320ce0e8e20dc4
Dusan Mijatovic (PC2016) <d.mijatovic@dv4all.nl>
2020-03-02 22:05:36 +0100

    added few e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,31 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+describe('Default Vue CLI page', () => {
+  before(()=>{
+    cy.visit('http://localhost:8080')
+  })
+
+  it('Has Vue.js in page title', () => {
+    cy.get("[data-test-id='page-title']")
+      .should("exist")
+      .contains("Vue.js")
+  })
+
+  it('Has cli plugins section title containing word Plugins',()=>{
+    cy.get("[data-test-id='cli-plugins-title']")
+      .should("exist")
+      .contains("Plugins")
+  })
+
+  it('Has essential links section title containing word Links',()=>{
+    cy.get("[data-test-id='essential-links-title']")
+      .should("exist")
+      .contains("Links")
+  })
+
+  it('Has ecosystem section title containing word Ecosystem',()=>{
+    cy.get("[data-test-id='ecosystem-title']")
+      .should("exist")
+      .contains("Ecosystem")
   })
 })
